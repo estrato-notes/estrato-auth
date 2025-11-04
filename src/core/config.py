be173e501fae05242ec0c582b40cc560c402ab1d
andre-maia51 <acmc.0410@gmail.com>
2025-11-04 09:17:16 -0300

    feat: add CORS configuration and middleware

--- src/core/config.py
+++ src/core/config.py
@@ -18,6 +18,8 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
+    CLIENT_ORIGIN_URL: str
+
     @property
     def DATABASE_URL(self) -> str:
         """Retorna a URL do banco de dados"""
--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 """Main do serviço de autenticação do projeto Estrato"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .core.config import settings
 
 # Router
 from .modules.auth.router import router as auth_router
 
 app = FastAPI(title="Estrato Auth API")
+
+origins = [settings.CLIENT_ORIGIN_URL]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth_router)
